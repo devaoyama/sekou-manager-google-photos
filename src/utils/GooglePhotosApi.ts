86d6c64310e3b80239ba8b385f783a2043c3061a
devkeita <keita.mywork@gmail.com>
2020-11-30 18:23:56 +0900

    アクセストークンが切れたらログアウトする

--- src/utils/GooglePhotosApi.ts
+++ src/utils/GooglePhotosApi.ts
@@ -1,3 +1,5 @@
+import { auth } from "./Firebase";
+
 export const getRequest = async (path: string, accessToken: string) => {
     const requestInit: RequestInit = {
         method: 'GET',
@@ -7,6 +9,7 @@ export const getRequest = async (path: string, accessToken: string) => {
     };
 
     const response = await fetch('https://photoslibrary.googleapis.com/v1/' + path, requestInit);
+    if (response.status === 401) await auth.signOut();
     return  await response.json();
 };
 
@@ -20,6 +23,7 @@ export const postRequest = async (path: string, accessToken: string, body: strin
     };
 
     const response = await fetch('https://photoslibrary.googleapis.com/v1/' + path, requestInit);
+    if (response.status === 401) await auth.signOut();
     return  await response.json();
 };
 
@@ -35,6 +39,7 @@ export const uploadFile = async (accessToken: string, image) => {
         body: image
     };
 
-    const res = await fetch('https://photoslibrary.googleapis.com/v1/uploads', requestInit)
-    return  await res.text();
+    const response = await fetch('https://photoslibrary.googleapis.com/v1/uploads', requestInit)
+    if (response.status === 401) await auth.signOut();
+    return  await response.text();
 };
