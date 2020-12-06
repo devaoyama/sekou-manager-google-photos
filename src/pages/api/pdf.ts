d97f614d393880052fd9e2fe34bba3639c1cc494
devkeita <keita.mywork@gmail.com>
2020-12-06 21:32:27 +0900

    add debug

--- src/pages/api/pdf.ts
+++ src/pages/api/pdf.ts
@@ -2,8 +2,15 @@ import { NowRequest, NowResponse } from "@vercel/node/dist";
 import makeTables from "../../utils/pdf/makeTables";
 import makeTop from "../../utils/pdf/makeTop";
 import merge from "../../utils/pdf/merge";
+import fs from "fs";
 
 export default async function handler(req: NowRequest, res: NowResponse) {
+    // カレントディレクトリ
+    const path = process.cwd();
+    // ファイル名の一覧
+    const filenames = fs.readdirSync(path);
+    console.log(filenames);
+
     const body = JSON.parse(req.body);
     const top = await makeTop(body.project);
     const tables = await makeTables(body.data, body.project.id);
