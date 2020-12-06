4d33439e4332d8228332cc6e530b046e7bc58eb7
devkeita <keita.mywork@gmail.com>
2020-12-06 21:48:32 +0900

    debug

--- src/pages/api/pdf.ts
+++ src/pages/api/pdf.ts
@@ -6,11 +6,11 @@ import fs from "fs";
 
 export default async function handler(req: NowRequest, res: NowResponse) {
     let path = process.cwd();
-    let filenames = fs.readdirSync(path + '/.next');
+    let filenames = fs.readdirSync(path + '/../');
     console.log(filenames);
 
     path = process.cwd();
-    filenames = fs.readdirSync(path + '/.next/serverless');
+    filenames = fs.readdirSync(path + '/.next/serverless/pages');
     console.log(filenames);
 
     const body = JSON.parse(req.body);
