8d0055c2fa879f0e31f5721389293582bed07cf9
devkeita <keita.mywork@gmail.com>
2020-12-06 21:26:37 +0900

    fix path

--- src/utils/pdf/make.ts
+++ src/utils/pdf/make.ts
@@ -1,10 +1,11 @@
+import path from "path";
 import fs from "fs";
 import labelmake from "labelmake";
 import { Template } from "labelmake/dist/types/type";
 
 const make = async (template: Template, inputs: any): Promise<Uint8Array> => {
     const font = {
-        "ipag": fs.readFileSync('fonts/ipag.ttf')
+        "ipag": fs.readFileSync(path.join(__dirname, 'fonts', 'ipag.ttf'))
     };
     return await labelmake({ inputs, template, font });
 };
