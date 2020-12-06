632f12a088f47baeb48b4840d0539e8c8b3e3626
devkeita <keita.mywork@gmail.com>
2020-12-06 21:00:57 +0900

    fixed directory

--- src/utils/pdf/make.ts
+++ src/utils/pdf/make.ts
@@ -3,8 +3,9 @@ import labelmake from "labelmake";
 import { Template } from "labelmake/dist/types/type";
 
 const make = async (template: Template, inputs: any): Promise<Uint8Array> => {
+    console.log(__dirname);
     const font = {
-        "ipag": fs.readFileSync('fonts/ipag.ttf')
+        "ipag": fs.readFileSync(__dirname + '/fonts/ipag.ttf')
     };
     return await labelmake({ inputs, template, font });
 };
