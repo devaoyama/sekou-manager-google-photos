73bc4979b64b547a35f9d9de8e4ceffa126b8b62
devkeita <keita.mywork@gmail.com>
2020-12-07 22:26:27 +0900

    remove api

--- src/components/PdfExportButton.tsx
+++ src/components/PdfExportButton.tsx
@@ -7,37 +7,37 @@ const PdfExportButton = ({ mediaItems, projectId }) => {
     const [loading, setLoading] = useState(false);
 
     const handleClick = async () => {
-        setLoading(true);
-        setLoading(true);
-        const doc = await db.collection('projects').doc(projectId).get();
-        const project: any = { id: projectId, ...doc.data() };
-        const data = [];
-        mediaItems.forEach(item => {
-            data.push({id: item.id, url: item.baseUrl});
-        });
-        const requestInit: RequestInit = {
-            mode: "no-cors",
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ data, project })
-        };
-        const response = await fetch('/api/pdf', requestInit);
-        if (response.status === 200) {
-            const jsonURL = window.URL.createObjectURL(await response.blob());
-            const link = document.createElement('a');
-            document.body.appendChild(link);
-            link.href = jsonURL;
-            link.setAttribute('download', `${project.title}.pdf`);
-            link.click();
-            document.body.removeChild(link);
-        }
-        setLoading(false);
+        // setLoading(true);
+        // setLoading(true);
+        // const doc = await db.collection('projects').doc(projectId).get();
+        // const project: any = { id: projectId, ...doc.data() };
+        // const data = [];
+        // mediaItems.forEach(item => {
+        //     data.push({id: item.id, url: item.baseUrl});
+        // });
+        // const requestInit: RequestInit = {
+        //     mode: "no-cors",
+        //     method: 'POST',
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: JSON.stringify({ data, project })
+        // };
+        // const response = await fetch('', requestInit);
+        // if (response.status === 200) {
+        //     const jsonURL = window.URL.createObjectURL(await response.blob());
+        //     const link = document.createElement('a');
+        //     document.body.appendChild(link);
+        //     link.href = jsonURL;
+        //     link.setAttribute('download', `${project.title}.pdf`);
+        //     link.click();
+        //     document.body.removeChild(link);
+        // }
+        // setLoading(false);
     };
 
     return (
-        <ListItem button onClick={handleClick} disabled={loading}>
+        <ListItem button onClick={handleClick} disabled>
             <ListItemIcon>
                 <PictureAsPdfIcon />
             </ListItemIcon>
